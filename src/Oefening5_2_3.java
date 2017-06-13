46f3831e66ef4a52609d600024016121db0fe375
kleendevos <kleen.devos@gmail.com>
2017-06-13 19:02:35 +0200

    Create Oefening5_2_3.java

--- src/Oefening5_2_3.java
+++ src/Oefening5_2_3.java
@@ -1,24 +1,32 @@
 import java.util.Scanner;
 
-/**
- * Created by vdabcursist on 13/06/2017.
- */
 public class Oefening5_2_3 {
     public static void main (String [] args){
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("NAAM: ");
         String naam = sc.nextLine();
+        System.out.println("PERSONEELSNUMMER: ");
         int personeelsnummer = sc.nextInt();
+        System.out.println("GEWERKTE UREN: ");
         double urengewerkt = sc.nextDouble();
+        System.out.println("GEWERKTE MINUTEN: ");
+        double minutengewerkt = sc.nextDouble();
+        System.out.println("UURLOON: ")
         int uurloon = sc.nextInt();
+        
+        double totaalgewerkteuren = ((urengewerkt * 60) + minutengewerkt)/60
 
 
-        if (urengewerkt<40){
-            double resultaat = uurloon * urengewerkt;
+        if (totaalgewerkteuren<=40){
+            double resultaat = uurloon * totaalgewerkteuren;
+            System.out.println ("MAANDLOON =" + resultaat);
         } else {
-            double resultaat = (uurloon * 40)+((urengewerkt-40)*(uurloon*1.5));
+            double resultaat = (uurloon * 40)+((totaalgewerkteuren-40)*(uurloon*1.5));
+            System.out.println ("MAANDLOON =" + resultaat);
         }
 
     }
+        
 }
